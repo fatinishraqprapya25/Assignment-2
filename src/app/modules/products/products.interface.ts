8be6e82d7be0a6469d8747cc73ee697c549f9cd6
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-06-28 11:10:10 +0600

    products interface design done

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,22 @@
+
+export interface Variant {
+    type: string;
+    value: string;
+}
+
+export interface Inventory {
+    quantity: number;
+    inStock: true;
+}
+
+export interface Products {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: Variant[];
+    inventory: Inventory;
+}
+
+export default Products;
