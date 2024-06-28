df1e108a6242389ac581d3e6d808659624ff8ec1
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-06-28 20:14:19 +0600

    service for orders done

---
+++ src/app/modules/orders/orders.service.ts
@@ -0,0 +1,34 @@
+import { Order } from "./orders.interface";
+import OrderModel from "./orders.model";
+
+// Creating Order
+const createOrderIntoDb = async (order: Order) => {
+    try {
+        const result = await OrderModel.create(order);
+        return result;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
+}
+
+// Retreiving all Orders
+const retrievingAllOrderFromDb = async () => {
+    try {
+        const result = await OrderModel.find({});
+        return result;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
+}
+
+// Retreiving orders by Email Address
+const retrievingOrdersByEmailFromDb = async (email: string) => {
+    try {
+        const result = await OrderModel.find({ email });
+        return result;
+    } catch (err: any) {
+        throw new Error(err.message);
+    }
+}
+
+export const ordersService = { createOrderIntoDb, retrievingAllOrderFromDb, retrievingOrdersByEmailFromDb };
