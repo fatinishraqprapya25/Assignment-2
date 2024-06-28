58ba44a159a95216f65b951c7cfb98c0a4e1efe8
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-06-28 19:59:27 +0600

    order router setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import productRouter from "./app/modules/products/products.route";
+import orderRouter from "./app/modules/orders/orders.route";
 
 const app: Application = express();
 
@@ -10,5 +11,6 @@ app.use(cors());
 
 // Application Routes
 app.use("/api/products", productRouter);
+app.use("api/orders", orderRouter);
 
 export default app;
