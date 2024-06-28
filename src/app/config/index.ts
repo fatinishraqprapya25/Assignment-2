fdc225e7d78ef62051290873c0b3cf36e16e621a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2024-06-28 10:37:48 +0600

    server setup done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
     port: process.env.PORT,
-    db_url: process.env.DATABASE_URL
+    db_url: process.env.DATABASE_URL as string
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
+import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
 
 async function main() {
-    app.listen(config.port, () => {
-        console.log("Server running at port ", config.port);
-    });
+    try {
+        // database connection
+        mongoose.connect(config.db_url);
+
+        // running server
+        app.listen(config.port, () => {
+            console.log("Server running at port ", config.port);
+        });
+    } catch (err) {
+        throw new Error("Server Internal Error Occured");
+    }
 }
 
 main();
